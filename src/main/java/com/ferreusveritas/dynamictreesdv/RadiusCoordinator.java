03367693815ab8dcd4a383f8b6d57725fb170fdc
ferreusveritas <ferreusveritas@gmail.com>
2018-10-29 10:11:56 -0400

    update to latest DT

--- src/main/java/com/ferreusveritas/dynamictreesdv/RadiusCoordinator.java
+++ src/main/java/com/ferreusveritas/dynamictreesdv/RadiusCoordinator.java
@@ -5,17 +5,29 @@ import java.util.Random;
 import com.ferreusveritas.dynamictrees.api.worldgen.IRadiusCoordinator;
 
 public class RadiusCoordinator implements IRadiusCoordinator {
-
+	
 	private final Random random;
+	
+	public int packing = 0;
 	
 	public RadiusCoordinator(Random random) {
 		this.random = random;
 	}
 	
 	@Override
-	public int getRadiusAtCoords(double x, double z) {
-		return 5;
-		//return random.nextInt(2) + 7;
+	public int getRadiusAtCoords(int x, int z) {
+		switch(packing) {
+			default:
+			case 0: return random.nextInt(7) + 2;
+			case 1: return 3;
+			case 2: return 2;
+		}
+	}
+	
+	@Override
+	public boolean setPacking(int chunkX, int chunkZ, int iteration) {
+		this.packing = iteration;
+		return packing < 3;
 	}
 	
 }
